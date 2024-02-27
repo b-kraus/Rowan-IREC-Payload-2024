0a6737025f0ab9f53ae9563e3c168bb675ea1689
Beth Kraus <krausb87@students.rowan.edu>
2024-02-27 13:35:32 -0500

    Creating Database and Tables for each sensor

--- Database.py
+++ Database.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+conn = sqlite3.connect("payload_sensors.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXIST ADXL345BCCZ (x,y,z)")
+cursor.execute("CREATE TABLE IF NOT EXIST MQ4 (LPG, CO)")
+cursor.execute("CREATE TABLE IF NOT EXIST BME680 (Temperature, Gas, Humidity, Pressure, Altitude)")
+cursor.execute("CREATE TABLE IF NOT EXIST AM2320 (Temperature, Humidity)")
+
