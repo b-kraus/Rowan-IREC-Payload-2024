edbd58e34eb6bc56d29301ea80c593cc5984e6a4
Beth Kraus <krausb87@students.rowan.edu>
2024-04-09 14:13:17 -0400

    Python Update

--- Analysis/Database.py
+++ Analysis/Database.py
@@ -1,9 +1,4 @@
-import sqlite3
-
-conn = sqlite3.connect("payload_sensors.db")
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXIST ADXL345BCCZ (x,y,z)")
-cursor.execute("CREATE TABLE IF NOT EXIST MQ4 (LPG, CO)")
-cursor.execute("CREATE TABLE IF NOT EXIST BME680 (Temperature, Gas, Humidity, Pressure, Altitude)")
-cursor.execute("CREATE TABLE IF NOT EXIST AM2320 (Temperature, Humidity)")
+import pandas as pd
+xyz = pd.read_excel('NewSheet1975.xlsx')
+print(xyz)
 
