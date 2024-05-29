6c4c6f14e4e725806aef4b187fd3822f1a2cd025
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-05-29 19:32:15 -0400

    Update Final_code.py

--- Final_code.py
+++ Final_code.py
@@ -9,20 +9,30 @@ import adafruit_bme680
 
 i2c = board.I2C()
 
-
 while True:
-    
+    f = open("accceleration.csv", newline = "")
+    rc = csv.writer(f)
     accelerometer = adafruit_adxl34x.ADXL343(i2c)
-    print("%f %f %f" % accelerometer.acceleration)
+    rc.writerow(["%f %f %f" % accelerometer.acceleration])
     time.sleep(0.2)
- 
+
     bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
     bme680.sea_level_pressure = 1013.25
     temperature_offset = -5
-    print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
-    print("Altitude = %0.2f meters" % bme680.altitude)
+    f = open("Temperature.csv", newline = "")
+    rc = csv.writer(f)
+    rc.writerow(["\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset)])
+    f = open("Gas.csv", newline = "")
+    rc = csv.writer(f)
+    rc.writerow(["Gas: %d ohm" % bme680.gas])
+    f = open("Humidity.csv", newline = "")
+    rc = csv.writer(f)
+    rc.writerow([("Humidity: %0.1f %%" % bme680.relative_humidity])
+    f = open("Pressure.csv", newline = "")
+    rc = csv.writer(f)
+    rc.writerow(["Pressure: %0.3f hPa" % bme680.pressure])
+    f = open("Altitude.csv", newline = "")
+    rc = csv.writer(f)
+    rc.writerow(["Altitude = %0.2f meters" % bme680.altitude])
     time.sleep(.3)
 
