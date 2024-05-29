04f6d5cf940069bf0ac6a0a3bca1310293d119cd
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-05-29 19:12:06 -0400

    Update Final_code.py

--- Final_code.py
+++ Final_code.py
@@ -7,22 +7,22 @@ import sys
 import adafruit_adxl34x
 import adafruit_bme680
 
+i2c = board.I2C()
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-
-# For ADXL343
-accelerometer = adafruit_adxl34x.ADXL343(i2c)
-# For ADXL345
-# accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 while True:
+    
+    accelerometer = adafruit_adxl34x.ADXL343(i2c)
     print("%f %f %f" % accelerometer.acceleration)
     time.sleep(0.2)
+ 
+    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+    bme680.sea_level_pressure = 1013.25
+    temperature_offset = -5
+    print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
+    print("Gas: %d ohm" % bme680.gas)
+    print("Humidity: %0.1f %%" % bme680.relative_humidity)
+    print("Pressure: %0.3f hPa" % bme680.pressure)
+    print("Altitude = %0.2f meters" % bme680.altitude)
+    time.sleep(.3)
 
-#BME
-i2c = board.I2C()  # uses board.SCL and board.SDA
-bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
-bme680.sea_level_pressure = 1013.25
-
-time.sleep(1)
