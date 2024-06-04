cc2daed7e342d91721faa9cbce5dbce996cf83b8
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-06-04 19:03:27 -0400

    Update GasTemperature.py

--- GasTemperature.py
+++ GasTemperature.py
@@ -9,17 +9,17 @@ import matplotlib.pyplot as plt
 i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
-def create_temperature_graph(): 
+def create_gas_graph(): 
   fig = plt.figure()
   ax = fig.add_subplot(1,1,1)
   xs = []
   ys = []
 
-def temperaturegraphs():
+def gasgraphs():
   xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
   ys.append(bme680.gas)
    
-def draw_temperaturegraphs():
+def draw_gasgraphs():
   ax.plot(xs, ys)
   plt.xticks(rotation=45, ha='right')
   plt.subplots_adjust(bottom=0.30)
