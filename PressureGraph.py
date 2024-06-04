2c32376af7e7c4d20624ab2250a9563ea87f1310
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-06-04 19:13:33 -0400

    Create PressureGraph.py

---
+++ PressureGraph.py
@@ -0,0 +1,28 @@
+import time
+import board
+import busio
+import sys
+import adafruit_bme680
+import datetime as dt
+import matplotlib.pyplot as plt
+
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+
+def create_pressure_graph(): 
+  fig = plt.figure()
+  ax = fig.add_subplot(1,1,1)
+  xs = []
+  ys = []
+
+def pressuregraphs():
+  xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+  ys.append(bme680.pressure)
+   
+def draw_pressure_graphs():
+  ax.plot(xs, ys)
+  plt.xticks(rotation=45, ha='right')
+  plt.subplots_adjust(bottom=0.30)
+  plt.title('Pressure over Time')
+  plt.ylabel('Pressure Pa')
+  plt.savefig('pressure.png', dpi = 300, bbox_inches='tight')
