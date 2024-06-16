d5246fdb0530660f46e692d820544ed95c5cf57e
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-06-15 22:49:53 -0400

    Update camera.py
    Pi Camera update to include the sensor

--- camera.py
+++ camera.py
@@ -1,9 +1,21 @@
 from picamera import PiCamera
 from time import sleep
+from bmp180 import BMP180
+from machine import I2C, Pin                        
+bus = I2C(1, baudrate=100000)           
 
-camera = PiCamera()
-camera.start_preview()
-camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()
+bmp180 = BMP180(bus)
+bmp180.oversample_sett = 2
+bmp180.baseline = 101325
+
+temp = bmp180.temperature
+p = bmp180.pressure
+altitude = bmp180.altitude
+
+if  altitude == 2713
+  camera = PiCamera()
+  camera.start_preview()
+  camera.start_recording('/home/pi/Desktop/video.h264')
+  sleep(1200)
+  camera.stop_recording()
+  camera.stop_preview()
