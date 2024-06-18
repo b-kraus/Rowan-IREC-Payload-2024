05dcf0448747c82f88bdd154c24db2eb031a3366
Beth Kraus <122136231+b-kraus@users.noreply.github.com>
2024-06-17 21:57:44 -0400

    Update camera.py
    cleaning up code

--- camera.py
+++ camera.py
@@ -7,15 +7,21 @@ bus = I2C(1, baudrate=100000)
 bmp180 = BMP180(bus)
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
+try:
+  while True:
+    temp = bmp180.temperature
+    p = bmp180.pressure
+    altitude = bmp180.altitude
 
-temp = bmp180.temperature
-p = bmp180.pressure
-altitude = bmp180.altitude
-
-if  altitude == 2713
-  camera = PiCamera()
-  camera.start_preview()
-  camera.start_recording('/home/pi/Desktop/video.h264')
-  sleep(1200)
-  camera.stop_recording()
-  camera.stop_preview()
+    if  altitude == 2713:
+      camera = PiCamera()
+      camera.start_preview()
+      camera.start_recording('/home/pi/Desktop/video.h264')
+      sleep(1200)
+      camera.stop_recording()
+      camera.stop_preview()
+      camera.close()
+      break
+finally: 
+  bus.close()
+  
